ea5e4540057914a7fde4d3a9940c93b0a579411f
David Medina <Medinadavid@gmail.com>
2025-03-12 14:05:41 +0100

    Arreglo error tarifas pricing

--- src/app/tarifas/page.tsx
+++ src/app/tarifas/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from "@/contexts/authContext"
 import ProtectedRoute from '@/components/ProtectedRoute/protectedRoute';
-import '@/components/Styles/Styles.css'
+import '@/components/Styles/styles.css'
 
 const Membership: React.FC = () => {
   const [hovered, setHovered] = useState([false, false, false]);
@@ -22,7 +22,7 @@ const Membership: React.FC = () => {
     setHovered(newHovered);
   };
 
-  const togglePricing = () => {
+  const togglePricing =() => {
     setIsAnnual(!isAnnual);
   };
 
@@ -44,20 +44,20 @@ const Membership: React.FC = () => {
     },
   };
 
-
-// la línea de abajo, antes estaba así =>  const handlePlanSelection = async (planType: string) => {
-
-  const handlePlanSelection = async (planType: 'free' | 'premium' | 'pro' | 'weekly') => {
+  const handlePlanSelection = async (planType: string) => {
     if (!user) {
       // Si no hay usuario, redirigir a login
       window.location.href = '/login';
       return;
     }
 
-    // Obtener el precio correspondiente según el tipo de plan y el estado anual
-    let planPrice = isAnnual
-      ? pricing[planType].annual
-      : pricing[planType].monthly;
+    // Obtener el precio correspondiente según el tipo de plan
+    let planPrice = 0;
+    if (planType === 'premium') {
+      planPrice = pricing.premium.monthly;  // Solo tomamos el precio mensual por ahora
+    } else if (planType === 'pro') {
+      planPrice = pricing.pro.monthly;  // Solo tomamos el precio mensual por ahora
+    }
 
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/order`, {
@@ -93,14 +93,16 @@ const Membership: React.FC = () => {
   }, [user]);
 
   return (
-    //<ProtectedRoute>
     <div className="flex items-center justify-center p-0">
       <div className="text-center">
         <h1 className="text-4xl mb-12">Tarifas</h1>
-
-        <div className="flex items-center justify-center mb-8 space-x-8">
+  
+        {/* Contenedor flex para alinear el texto y el switch */}
+        <div className="flex items-center justify-center mb-8 space-x-8"> {/* space-x-8 para espaciar los elementos */}
+          
+          {/* Texto a la izquierda (Semanal) */}
           <span className="text-lg sm:text-xl mr-3">Mensual</span>
-
+  
           {/* Switch */}
           <div className="love">
             <input 
@@ -116,26 +118,27 @@ const Membership: React.FC = () => {
               <div className="round"></div>
             </label>
           </div>
-
+  
+          {/* Texto a la derecha (Anual) */}
           <span className="text-lg sm:text-xl">Anual</span>
         </div>
 
         <div className="flex justify-start items-start flex-wrap -mx-2 mb-8">
-          {/* Gratis Plan */}
+          {/* Gratis Plan
           <div
             className="tarifa-card bg-white border-2 border-black rounded-lg w-64 h-[500px] p-3 text-left flex flex-col justify-between transition-all transform hover:translate-y-[-10px] hover:shadow-lg hover:shadow-blue-200/50 mx-2 hover:border-blue-200 hover:bg-blue-50 mb-6"
             onMouseEnter={() => handleMouseEnter(0)}
             onMouseLeave={() => handleMouseLeave(0)}
-          >
+            >
             <h2 className="text-xl font-semibold mb-0">Gratis</h2>
             <p className="text-sm mt-0 mb-0">Perfecto para usuarios ocasionales o para aquellas personas que quieran conocer myCloset.</p>
             <p className="text-2xl font-bold mt-0 mb-0">
-              {pricing.free[isAnnual ? 'annual' : 'monthly']} <span className="text-base font-bold">$</span>
+              {pricing.free.monthly} <span className="text-base font-bold">$</span>
             </p>
             <button
               onClick={() => window.location.href = '/login'} 
               className="mt-4 w-full border-2 border-blue-500 bg-white text-blue-500 py-2 px-4 rounded-lg hover:bg-blue-500 hover:text-white"
-            >
+              >
               ¡Crear cuenta!
             </button>
             <div className="border-t border-gray-300 my-0"></div>
@@ -148,10 +151,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  Acceso limitado a  las funciones.
+                  Acceso limitado a funciones.
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -160,10 +163,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  Sin capacidad de guardar tus outfits.
+                  No tendrás armario.
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -172,32 +175,31 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  ¡Para probar la plataforma!
+                  ¡Ideal para probar la plataforma!
                 </li>
               </ul>
             </div>
-          </div>
+          </div> */}
 
           {/* Premium Plan */}
           <div
             className="tarifa-card bg-white border-2 border-black rounded-lg w-64 h-[500px] p-3 text-left flex flex-col justify-between transition-all transform hover:translate-y-[-10px] hover:shadow-lg hover:shadow-blue-200/50 mx-2 hover:border-blue-200 hover:bg-blue-50 mb-6"
             onMouseEnter={() => handleMouseEnter(1)}
             onMouseLeave={() => handleMouseLeave(1)}
-          >
+            >
             <h2 className="text-xl font-semibold mb-0">Premium</h2>
             <p className="text-sm mt-0 mb-0">Acceso completo a funcionalidades premium. Incluye soporte prioritario y contenido exclusivo.</p>
             <p className="text-2xl font-bold mt-0 mb-0">
-              {pricing.premium[isAnnual ? 'annual' : 'monthly']} <span className="text-base font-bold">$</span>
-              <span className="text-base font-bold">{isAnnual ? '/ año' : '/ mes'}</span>
+              {pricing.premium[isAnnual ? 'annual' : 'monthly']} <span className="text-base font-bold">$</span><span className="text-base font-bold">/ mes</span>
             </p>
             <button 
               onClick={() => handlePlanSelection('premium')}
               className="mt-4 w-full border-2 border-blue-500 bg-white text-blue-500 py-2 px-4 rounded-lg hover:bg-blue-500 hover:text-white"
-            >
-              ¡Continuar!
+              >
+              Continuar
             </button>
             <div className="border-t border-gray-300 my-0"></div>
             <div className="text-left flex-grow-0 mt-2 pt-0">
@@ -209,7 +211,7 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
                   Acceso completo a la plataforma.
@@ -221,10 +223,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  Capacidad de guardar 10 outfits. 
+                  Soporte prioritario.
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -233,10 +235,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  ¡Contenido exclusivo y más!
+                  ¡Podrás guardar hasta 10 outfits!
                 </li>
               </ul>
             </div>
@@ -247,7 +249,7 @@ const Membership: React.FC = () => {
             className="tarifa-card bg-white border-2 border-black rounded-lg w-64 h-[500px] p-3 text-left flex flex-col justify-between transition-all transform hover:translate-y-[-10px] hover:shadow-lg hover:shadow-blue-200/50 mx-2 hover:border-blue-200 hover:bg-blue-50 mb-6"
             onMouseEnter={() => handleMouseEnter(2)}
             onMouseLeave={() => handleMouseLeave(2)}
-          >
+            >
             <h2 className="text-xl font-semibold mb-0">Pro</h2>
             <p className="text-sm mt-0 mb-0">Acceso completo a todo el contenido y soporte prioritario las 24 horas.</p>
             <p className="text-2xl font-bold mt-0 mb-0">
@@ -256,8 +258,8 @@ const Membership: React.FC = () => {
             <button 
               onClick={() => handlePlanSelection('pro')}
               className="mt-4 w-full border-2 border-blue-500 bg-white text-blue-500 py-2 px-4 rounded-lg hover:bg-blue-500 hover:text-white"
-            >
-              ¡Continuar!
+              >
+              Continuar
             </button>
             <div className="border-t border-gray-300 my-0"></div>
             <div className="text-left flex-grow-0 mt-2 pt-0">
@@ -269,10 +271,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  Contenido exclusivo ilimitado. 
+                  Acceso completo a todas las características.
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -281,10 +283,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  Funcionalidad de clima añadida.
+                  Acceso exclusivo a las actualizaciones de productos.
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -293,10 +295,10 @@ const Membership: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  >
+                    >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                   </svg>
-                  ¡Sin límite ninguno!
+                  ¡Función de clima añadida! 
                 </li>
               </ul>
             </div>
@@ -304,6 +306,7 @@ const Membership: React.FC = () => {
         </div>
       </div>
     </div>
+                    //</ProtectedRoute>
   );
 };
 
